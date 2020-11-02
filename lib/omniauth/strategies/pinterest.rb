7847fa5883104ec24d2c9f4b2ef82fed62c0ceb4
Jeff Chen <jeff@pixleeteam.com>
2020-11-01 19:10:08 -0800

    upgrade for pinterest api v3

--- lib/omniauth/strategies/pinterest.rb
+++ lib/omniauth/strategies/pinterest.rb
@@ -4,13 +4,13 @@ module OmniAuth
   module Strategies
     class Pinterest < OmniAuth::Strategies::OAuth2
       option :client_options, {
-        :site => 'https://api.pinterest.com/',
-        :authorize_url => 'https://api.pinterest.com/oauth/',
-        :token_url => 'https://api.pinterest.com/v1/oauth/token'
+        :site => 'https://www.pinterest.com/',
+        :authorize_url => 'https://www.pinterest.com/oauth/',
+        :token_url => 'https://api.pinterest.com/v3/oauth/access_token/'
       }
 
       def request_phase
-        options[:scope] ||= 'read_public'
+        options[:scope] ||= 'read_pins'
         options[:response_type] ||= 'code'
         super
       end
@@ -19,6 +19,10 @@ module OmniAuth
 
       info { raw_info }
       
+      def callback_url
+        full_host + script_name + callback_path
+      end
+
       def authorize_params
         super.tap do |params|
           %w[redirect_uri].each do |v| 
